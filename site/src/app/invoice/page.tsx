f8437c8cc71e6f3ca827520f53a4a837ae2a7d3a
skoltecurethekids <skolte@curethekids.org>
2026-05-12 21:16:30 -0700

    Add trailing slash to invoice API endpoint

--- site/src/app/invoice/page.tsx
+++ site/src/app/invoice/page.tsx
@@ -1,7 +1,7 @@
 export default function InvoicePage() {
   return (
     <iframe
-      src="/api/invoice-html"
+      src="/api/invoice-html/"
       style={{
         width: '100%',
         height: '100vh',
